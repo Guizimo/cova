b6ac7d45f16b9902f0dda2e2c3dc2af06be53249
Guizimo <17680262548@163.com>
2025-04-15 16:21:18 +0800

    refactor: 移除文档相关文件并更新配置
    移除与文档相关的所有文件，包括图片、Markdown 文件、样式和组件。

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite"
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   // 只在生产环境下设置 base
   base: command === 'serve' ? '/' : '/cova/',
   resolve: {
